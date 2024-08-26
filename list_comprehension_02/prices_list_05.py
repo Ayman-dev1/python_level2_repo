cae075e078e30040dca14378a48ca9cb85714d7e
Ayman-dev1 <aymanelwan67@gmail.com>
2024-08-26 08:33:59 +0300

    list comprehension update

--- list_comprehension_02/prices_list_05.py
+++ list_comprehension_02/prices_list_05.py
@@ -18,14 +18,15 @@ valid_prices_list = [item if item > 0 else 'invalid price' for item in prices_li
 
 print(valid_prices_list)
 
-
 print('--------------- convert this code to a function and use it in a list comprehension  ---------------------')
+
 
 def check_price(price):
     if price > 0:
         return price
     else:
         return 'invalid price'
+
 
 prices_list = [300, 150, 160, -80, 250, -60, 400]
 valid_prices_list = [check_price(item) for item in prices_list]
